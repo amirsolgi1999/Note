dbec48a8cdf90bbe6d1fc15cf4be660a0041ccbf
A.solgi <amirsolgiiii1999@gmail.com>
2025-03-15 18:10:30 +0330

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
-
 android {
     namespace = "com.example.note"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.note"
@@ -66,4 +67,51 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+
+    // compose dependencies
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation ("androidx.navigation:navigation-compose:2.8.9")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+
+    // dagger - hilt
+    implementation ("com.google.dagger:hilt-android-gradle-plugin:2.55")
+    implementation ("com.google.dagger:hilt-android:2.55")
+    ksp ("com.google.dagger:hilt-android-compiler:2.52")
+    ksp ("androidx.hilt:hilt-compiler:1.2.0")
+
+    // room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    ksp ("androidx.room:room-compiler:2.6.1")
+
+    // kotlin extensions and coroutines support for room
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // material 3
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation ("androidx.compose.material:material:1.7.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    // live data
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+
+    // for tab layout draggable
+    implementation ("androidx.compose.runtime:runtime-livedata:1.7.8")
+
+    // data store
+    implementation("androidx.datastore:datastore-preferences:1.1.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    // splash API
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    // lottie
+    implementation("com.airbnb.android:lottie-compose:6.1.0")
 }
--- app/src/main/java/com/example/note/ui/theme/Color.kt
+++ app/src/main/java/com/example/note/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.example.note.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+
+val Primary = Color(0x4D2F2F2F)
+val OnPrimary = Color(0xffF6D14C)
+val secondary =Color(0xffF6ECC9)
+val Surface = Color(0xff181818)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
